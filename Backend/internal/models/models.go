490b82a370d57984b2aca9a2d2cfa553675abddc
deeep8250 <deepmondal825005@gmail.com>
2025-12-26 11:06:20 +0530

    models

--- Backend/internal/models/models.go
+++ Backend/internal/models/models.go
@@ -1,6 +1,11 @@
+//here we use polymorphic design
+// : Polymorphic design is when a table uses the same column(s) to reference multiple different parent tables.
+
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type Login struct {
 	Email    string `json:"email" binding:"required,email"`
@@ -13,18 +18,38 @@ type SignUp struct {
 	Password string `json:"password"  binding:"required,min=6"`
 }
 
-type CreateTaskRequest struct {
-	TaskName string `json:"task_name" binding:"required"`
-	Task     string `json:"task" binding:"required"`
-}
-
 type User struct {
 	Id           uint           `json:"id" gorm:"primaryKey"`
 	Name         string         `json:"name" gorm:"not null"`
 	Email        string         `json:"email" gorm:"unique;not null"`
+	Phone        int            `json:"phone" gorm:"unique; not null"`
+	Location     Location       `gorm:"foreignKey:OwnerId"`
 	Password     string         `json:"-" gorm:"not null"`
+	CreatedAt    time.Time      `json:"created_at" gorm:"not null"`
 	RefreshToken []RefreshToken `json:"-" gorm:"foreignKey:UserId"`
-	Tasks        []Task         `json:"tasks,omitempty" gorm:"foreignKey:UserID"`
+}
+
+type Location struct {
+	Id        uint      `json:"id" gorm:"primaryKey"`
+	OwnerId   uint      `json:"user_id"`
+	Pin       int64     `json:"pin" gorm:"not null" binding:"min=6 , max=6"`
+	State     string    `json:"state" gorm:"not null"`
+	Village   string    `json:"village" gorm:"not null"`
+	OwnerType string    `json:"owner_type" gorm:"not null"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"not null"`
+}
+
+type Club struct {
+	Id          uint      `json:"id" gorm:"primaryKey"`
+	UserId      uint      `json:"user_id" gorm:"not null"`
+	Club_name   string    `json:"club_name" gorm:"not null"`
+	AadharNb    int64     `json:"aadhar_nb" gorm:"not null" binding:"min=12 max=12"`
+	PublishedAt time.Time `json:"published_at" gorm:"not null"`
+	Is_Verified bool      `json:"is_verified" gorm:"not null"`
+	Location    Location  `json:"location" gorm:"foreignKey:OwnerId"`
+	About       string    `json:"about" gorm:"not null"`
+	ProfileImg  string    `json:"profile_img" gorm:"not null"`
 }
 
 type RefreshToken struct {
@@ -33,15 +58,6 @@ type RefreshToken struct {
 	TokenHash string    `gorm:"not null"`
 	ExpireAt  time.Time `gorm:"not null"`
 	CreatedAt time.Time `gorm:"not null"`
-}
-
-type Task struct {
-	Id        uint      `json:"id" gorm:"primaryKey"`
-	TaskName  string    `json:"task_name"  gorm:"not null"`
-	Task      string    `json:"task" gorm:"not null"`
-	CreateAt  time.Time `json:"created_at"`
-	IsDeleted bool      `json:"is_deleted"`
-	UserID    uint      `gorm:"foreignKey"`
 }
 
 // When you add an index to a column, the database maintains a separate,
