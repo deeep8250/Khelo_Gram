ffdcf288944eb4ff852194c23296a0d1a792ac90
deeep8250 <deepmondal825005@gmail.com>
2025-12-29 21:17:19 +0530

    add middleware testing routes

--- Backend/internal/routes/routes.go
+++ Backend/internal/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"Notes/internal/handlers"
+	"Notes/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,5 +11,7 @@ func Routes(r *gin.Engine) {
 	r.POST("/login", handlers.Login)
 	r.POST("/sign_up", handlers.SignUp)
 	r.POST("/reset-refreshToken", handlers.GetRefreshToken)
+
+	r.GET("/user-info", middleware.AuthMiddleware(), handlers.ProtectedRoute)
 
 }
